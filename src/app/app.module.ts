b9703170fbe37be9a383ff7a0caa2e305117d6ae
ermaoL <284029652@qq.com>
2017-04-25 19:03:51 +0800

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,20 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent }  from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import {HeroService} from "./hero.service";
+import {DashboardComponent} from "./dashboard.component";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
-  imports:      [
+  imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([{
-      path: 'heroes',
-      component: HeroesComponent
-    }])],
-  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent ],
+    AppRoutingModule],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
   providers: [ HeroService ],
   bootstrap:    [ AppComponent ]
 })
